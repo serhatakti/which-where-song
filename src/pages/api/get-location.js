1d45855d8852599df4c1da7582eeab91182a2f05
serhatakti <serhatakti@gmail.com>
2024-06-15 01:51:42 +0300

    edge config added

--- src/pages/api/get-location.js
+++ src/pages/api/get-location.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+import {get} from '@vercel/edge-config';
 
-export default function handler(req, res) {
-  const params = {...req.query};
-  params.key=process.env.GOOGLE_API_KEY
-    params.radius=process.env.RADIUS
-  axios.get('/maps/api/place/nearbysearch/json',{params: params, baseURL:'https://maps.googleapis.com'})
-      .then(response => {
-        res.status(200).send(response.data)
-      })
-      .catch(err => res.status(400).send(err))
+export default async function handler(req, res) {
+    const params = {...req.query};
+    const radius=  await get('radius');
+    params.key = process.env.GOOGLE_API_KEY
+    params.radius = radius
+    axios.get('/maps/api/place/nearbysearch/json', {params: params, baseURL: 'https://maps.googleapis.com'})
+        .then(response => {
+            res.status(200).send(response.data)
+        })
+        .catch(err => res.status(400).send(err))
 }
